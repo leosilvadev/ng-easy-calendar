957cd7bd9775e80f7602cafe6d7e6a870b9b2037
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-08 22:49:54 -0300

    creating a new spec to check the end of the year

--- test/unit/currentCalendarSpec.js
+++ test/unit/currentCalendarSpec.js
@@ -2,9 +2,22 @@ describe('Days In factory Test', function() {
 
   beforeEach(module('easyCalendar.services'));
 
-  it('should return the correct days in september of 2015', inject(function(currentCalendar){
-    var days = daysIn(8, 2015);
-    expect(days.length).toBe(30);
+  it('should return the correct days when it is the last month in the year', inject(function(easyCalendarFactory){
+    var calendar = easyCalendarFactory(new Date(2015, 11, 1));
+
+    expect( calendar.days.length ).toBe( 35 );
+
+    expect( calendar.previous.getFullYear() ).toBe( 2015 );
+    expect( calendar.previous.getMonth() ).toBe( 10 );
+
+    expect( calendar.next.getFullYear() ).toBe( 2016 );
+    expect( calendar.next.getMonth() ).toBe( 0 );
+
+    expect( calendar.days[0].date.getDate() ).toBe( 29 );
+    expect( calendar.days[1].date.getDate() ).toBe( 30 );
+
+    expect( calendar.days[33].date.getDate() ).toBe( 1 );
+    expect( calendar.days[34].date.getDate() ).toBe( 2 );
   }));
 
 });
